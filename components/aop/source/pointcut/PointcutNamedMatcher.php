4db706a7e662bdf8a74f11245ff404f714ce5b79
mapi <mapi@43604d11-ced7-43f5-a9b8-bcb6bf537261>
2010-01-09 15:24:34 +0000

    # Missing doc comments added.
    git-svn-id: svn+ssh://xplib.de/var/svn/repos/buzz2ee.de/trunk@221 43604d11-ced7-43f5-a9b8-bcb6bf537261

--- components/aop/source/pointcut/PointcutNamedMatcher.php
+++ components/aop/source/pointcut/PointcutNamedMatcher.php
@@ -1,8 +1,48 @@
 <?php
 /**
- * I provide completely working code within this framework, which may not be
- * developed any further, because there are already existing packages, which
- * try to provide similar functionallities.
+ * This file is part of the aspect oriented programming component.
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2009-2010, Manuel Pichler <mapi@buzz2ee.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Components
+ * @package   de\buzz2ee\aop\pointcut
+ * @author    Manuel Pichler <mapi@buzz2ee.de>
+ * @copyright 2009-2010 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id$
+ * @link      http://buzz2ee.de/
  */
 
 namespace de\buzz2ee\aop\pointcut;
@@ -12,11 +52,15 @@ use de\buzz2ee\aop\interfaces\PointcutMatcher;
 use de\buzz2ee\aop\interfaces\PointcutRegistry;
 
 /**
- * Named pointcut reference matcher implementation.
  *
- * @author  Manuel Pichler <mapi@pdepend.org>
- * @license Copyright by Manuel Pichler
- * @version $Revision$
+ *
+ * @category  Components
+ * @package   de\buzz2ee\aop\pointcut
+ * @author    Manuel Pichler <mapi@buzz2ee.de>
+ * @copyright 2009-2010 Manuel Pichler. All rights reserved.
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   Release: @package_version@
+ * @link      http://buzz2ee.de/
  */
 class PointcutNamedMatcher implements PointcutMatcher
 {
